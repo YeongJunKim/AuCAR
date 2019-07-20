a85a87e3f33e90c693ed5de8d238c41f43a8ae44
colson <dud3722000@naver.com>
2019-07-20 20:23:19 +0900

    .

--- stm32cubeide/AuCAR_slave/Inc/stm32f4xx_it.h
+++ stm32cubeide/AuCAR_slave/Inc/stm32f4xx_it.h
@@ -57,6 +57,8 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void RCC_IRQHandler(void);
+void DMA1_Stream2_IRQHandler(void);
+void DMA1_Stream4_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
 void DMA1_Stream6_IRQHandler(void);
 void USART2_IRQHandler(void);
